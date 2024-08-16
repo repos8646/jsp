2a287fa1bbf5e9f7080fdeda62603cb7c1147891
repos8646 <repos8646@gmail.com>
2024-08-16 12:18:46 +0900

    ch10 study 240816

---
+++ ch10/src/main/java/service/User1Service.java
@@ -0,0 +1,39 @@
+package service;
+
+import java.util.List;
+
+import dao.User1DAO;
+import dto.User1DTO;
+
+public class User1Service {
+
+	private static User1Service instance = new User1Service();
+	public static User1Service getInstance() {
+		return instance;
+	}
+	private User1Service() {}
+	
+	// DAO 인스턴스
+	private User1DAO dao = User1DAO.getInstance();
+	
+	// DAO CRUD 메서드 호출
+	public void insertUser1(User1DTO dto) {
+		dao.insertUser1(dto);
+	}
+	
+	public User1DTO selectUser1(String uid) {
+		return dao.selectUser1(uid);
+	}
+	
+	public List<User1DTO> selecttUser1s() {
+		return dao.selecttUser1s();
+	}
+	
+	public void updateUser1(User1DTO dto) {
+		dao.updateUser1(dto);
+	}
+	
+	public void deleteUser1(String uid) {
+		dao.deleteUser1(uid);
+	}
+}
