55c99a7e9c64de36fdfb02aecdcf243b6b468f75
repos8646 <repos8646@gmail.com>
2024-08-20 17:49:35 +0900

    jboard study 240820-2

--- jboard/src/main/java/com/jboard/util/SQL.java
+++ jboard/src/main/java/com/jboard/util/SQL.java
@@ -3,9 +3,16 @@ package com.jboard.util;
 public class SQL {
 
 	public static final String SELECT_TERMS = "select * from terms";
+	
+	public static final String SELECT_COUNT_USER = "SELECT COUNT(*) FROM `user` ";
+	public static final String WHERE_UID  = "WHERE `uid`=?";
+	public static final String WHERE_NICK = "WHERE `nick`=?";
+	public static final String WHERE_EMAIL = "WHERE `email`=?";
+	public static final String WHERE_HP = "WHERE `hp`=?";
+	
 	public static final String INSERT_USER = "insert into user set "
 											+ "`uid`=?,"
-											+ "`pass`=?,"
+											+ "`pass`=SHA2(?, 256),"
 											+ "`name`=?,"
 											+ "`nick`=?,"
 											+ "`email`=?,"
